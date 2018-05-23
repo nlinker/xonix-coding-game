7dd00bbb8190e91003c5607c94612832e2b95f6e
Nick Linker <nlinker@gmail.com>
2018-05-23 14:35:27 +0600

    compile bug wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 pub mod utils;
 use utils::trim_indent;
+// use utils::get_add_function;
 
 fn main() {
     let gs0 = r#"
@@ -10,5 +11,31 @@ fn main() {
         *. . . . . .*.
         *.*.*.*.*.*.*.
     "#;
+    println!("{}", make_adder(1)(2));
+    println!("{}", make_adder(2)(3));
+    let indent = "xxx".to_ascii_lowercase();
+    let f = make_str_adder(&indent);
+    println!("add = {}", f("_abc_"));
     println!("trim_indent = {}", trim_indent(&gs0))
+}
+
+fn make_str_adder<'a>(a: &'a str) -> Box<Fn(&str) -> String + 'static> {
+    if a.chars().all(|c| c.is_ascii_whitespace()) {
+        Box::new(move |l| String::from(l))
+    } else {
+        Box::new(move |l| {
+            let mut r = String::new();
+            r.push_str(l);
+            r.push_str(a);
+            r
+        })
+    }
+}
+
+fn make_adder(a: i32) -> Box<Fn(i32) -> i32> {
+    if a == 1 {
+        Box::new(move |b| a + b)
+    } else {
+        Box::new(move |b| a * b)
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,13 +2,13 @@ extern crate itertools;
 
 use std::iter::Iterator;
 use self::itertools::Itertools;
-use std::slice::Iter;
+//use std::slice::Iter;
 
 pub fn trim_indent(src: &str) -> &str {
     replace_indent(&src, &"")
 }
 
-pub fn replace_indent<'a>(src: &'a str, new_indent: &str) -> &'a str {
+pub fn replace_indent<'a>(src: &'a str, new_indent: &'a str) -> &'a str {
     let lines = src.lines().collect_vec();
     let min_common_indent = lines.iter()
         .filter(|s| !is_blank(&s))
@@ -18,24 +18,20 @@ pub fn replace_indent<'a>(src: &'a str, new_indent: &str) -> &'a str {
     println!("min_common_indent = {:?}", min_common_indent);
     let exp_size = src.len() + new_indent.len() * lines.len();
     //let _: Iter<&str> = lines.iter();
-    let f1: Fn(&str) -> &str = get_add_function(&new_indent);
-    let f2: Fn(&str) -> &str = get_cut_function(min_common_indent);
-
+    //let f1: Box<Fn(&str) -> &str> = get_add_function(&new_indent);
+    //let f2: Box<Fn(&str) -> &str> = get_cut_function(min_common_indent);
     // reindent(lines.iter(), exp_size, f1, f2)
     src
 }
 
-pub fn get_add_function<F>(indent: &str) -> F
-    where F: Fn(&str) -> &str,
-{
-    |line: &str| format!("{}{}", indent, line)
-    // if indent.is_empty() { |l| l } else { |l| indent + l }
-}
-pub fn get_cut_function<F>(min_common_indent: usize) -> F
-    where F: Fn(&str) -> &str,
-{
-    |line: &str| { line[min_common_indent..] }
-}
+//pub fn get_add_function<'a>(indent: &'a str) -> Box<Fn(&str) -> &str + 'static> {
+//    Box::new(move |line: &str| &format!("{}{}", indent, line)[..] )
+//    // if indent.is_empty() { |l| l } else { |l| indent + l }
+//}
+//
+//pub fn get_cut_function(min_common_indent: usize) -> Box<Fn(&str) -> &str + 'static> {
+//    Box::new(move |line: &str| { &line[min_common_indent..] })
+//}
 
 //pub fn reindent<'a, F1, F2>(xs: Iter<&str>, exp_size: usize, indent_add_f: F1, indent_cut_f: F2) -> &'a str
 //    where
