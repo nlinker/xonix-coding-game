9ceb6598cc2c9de1763e62425888b1b6e60ef2bd
Nick Linker <nlinker@gmail.com>
2018-05-25 01:52:50 +0600

    the code compiles

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use utils::trim_indent;
 // use utils::get_add_function;
 
 fn main() {
+    let s = "aaa".trim_indent();
+    println!("{}", s);
     let gs0 = r#"
         *B*D*E*.*.*.*.
         *C . . . . .*.
@@ -11,31 +13,15 @@ fn main() {
         *. . . . . .*.
         *.*.*.*.*.*.*.
     "#;
-    println!("{}", make_adder(1)(2));
-    println!("{}", make_adder(2)(3));
-    let indent = "xxx".to_ascii_lowercase();
-    let f = make_str_adder(&indent);
-    println!("add = {}", f("_abc_"));
-    println!("trim_indent = {}", trim_indent(&gs0))
+    println!("trim_indent = {}", trim_indent(&gs0));
 }
 
-fn make_str_adder<'a>(a: &'a str) -> Box<Fn(&str) -> String + 'a> {
-    if a.chars().all(|c| c.is_ascii_whitespace()) {
-        Box::new(|l| String::from(l))
-    } else {
-        Box::new(move |l| {
-            let mut r = String::new();
-            r.push_str(l);
-            r.push_str(a);
-            r
-        })
-    }
+trait Tr {
+    fn trim_indent(self) -> Self;
 }
 
-fn make_adder(a: i32) -> Box<Fn(i32) -> i32> {
-    if a == 1 {
-        Box::new(move |b| a + b)
-    } else {
-        Box::new(move |b| a * b)
+impl<'a> Tr for &'a str {
+    fn trim_indent(self) -> Self {
+        &self[..1] // some transformation here
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -16,11 +16,11 @@ pub fn replace_indent<'a>(src: &'a str, new_indent: &'a str) -> &'a str {
         .min()
         .unwrap_or(0);
     println!("min_common_indent = {:?}", min_common_indent);
-    let exp_size = src.len() + new_indent.len() * lines.len();
+    let _exp_size = src.len() + new_indent.len() * lines.len();
     //let _: Iter<&str> = lines.iter();
-    let f1 = get_add_function(&new_indent);
-    let f2 = get_cut_function(min_common_indent);
-    reindent(lines.iter(), exp_size, f1, f2);
+    let _f1 = get_add_function(&new_indent);       // Box<Fn<(&str), Output=String>>
+    let _f2 = get_cut_function(min_common_indent); // Box<Fn<(&str), Output=String>>
+    // reindent(lines.iter(), exp_size, f1, f2);
     src
 }
 
@@ -32,29 +32,29 @@ pub fn get_cut_function(min_common_indent: usize) -> Box<Fn(&str) -> String> {
     Box::new(move |line: &str| { line[min_common_indent..].to_string() })
 }
 
-//pub fn reindent<'a, F1, F2>(xs: Iter<&str>,
-//                            exp_size: usize,
-//                            indent_add_f: Box<F1>,
-//                            indent_cut_f: Box<F2>) -> String
-//    where
-//        F1: Fn(&'a str) -> String,
-//        F1: Fn(&'a str) -> String,
-//{
-//    unimplemented!()
-////private inline fun List<String>.reindent(resultSizeEstimate: Int,
-////                                         indentAddFunction: (String) -> String,
-////                                         indentCutFunction: (String) -> String?): String {
-////    val lastIndex = lastIndex
-////    return mapIndexedNotNull { index, value ->
-////            if ((index == 0 || index == lastIndex) && value.isBlank())
-////                null
-////            else
-////                indentCutFunction(value)?.let(indentAddFunction) ?: value
-////        }
-////        .joinTo(StringBuilder(resultSizeEstimate), "\n")
-////        .toString()
-////}
+pub fn reindent<F1, F2>(_xs: Iter<&str>,
+                        _exp_size: usize,
+                        _indent_add_f: Box<F1>,
+                        _indent_cut_f: Box<F2>) -> String
+    where
+        F1: Fn(&str) -> String + Sized,
+        F1: Fn(&str) -> String + Sized,
+{
+    unimplemented!()
+//private inline fun List<String>.reindent(resultSizeEstimate: Int,
+//                                         indentAddFunction: (String) -> String,
+//                                         indentCutFunction: (String) -> String?): String {
+//    val lastIndex = lastIndex
+//    return mapIndexedNotNull { index, value ->
+//            if ((index == 0 || index == lastIndex) && value.isBlank())
+//                null
+//            else
+//                indentCutFunction(value)?.let(indentAddFunction) ?: value
+//        }
+//        .joinTo(StringBuilder(resultSizeEstimate), "\n")
+//        .toString()
 //}
+}
 
 pub fn indent_width(s: &str) -> usize {
     s.chars().position(|c| !c.is_ascii_whitespace()).unwrap_or(s.len())
