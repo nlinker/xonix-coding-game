c42e2fea952d9416cd2e11c290ec7e7161fa2c6a
Nick Linker <nlinker@gmail.com>
2018-07-04 20:20:47 +0600

    impl Iterator use

--- tests/test_game.rs
+++ tests/test_game.rs
@@ -35,20 +35,20 @@ mod test {
         //   * * * * * * *
         for (m, n) in field_sizes {
             let mut perimeter: Vec<Point> = vec![];
-            perimeter.append(&mut points_2(0, Box::new(0..n)));
-            perimeter.append(&mut points_1(Box::new(1..m - 1), n - 1));
-            perimeter.append(&mut points_2(m - 1, Box::new((0..n).rev())));
-            perimeter.append(&mut points_1(Box::new((1..m - 1).rev()), 0));
+            perimeter.append(&mut points_2(0, 0..n));
+            perimeter.append(&mut points_1(1..m - 1, n - 1));
+            perimeter.append(&mut points_2(m - 1, (0..n).rev()));
+            perimeter.append(&mut points_1((1..m - 1).rev(), 0));
             let size = 2 * (m + n - 2) as usize;
             assert_eq!(size, perimeter.len());
             for l in 0..=(2*size) {
                 assert_eq!(perimeter[l % size], border_to_point(m as usize, n as usize, l as usize));
             }
         }
-        fn points_1(ii: Box<Iterator<Item=i16>>, j: i16) -> Vec<Point> {
+        fn points_1(ii: impl Iterator<Item=i16>, j: i16) -> Vec<Point> {
             ii.map(|i| Point(i, j)).collect()
         }
-        fn points_2(i: i16, jj: Box<Iterator<Item=i16>>) -> Vec<Point> {
+        fn points_2(i: i16, jj: impl Iterator<Item=i16>) -> Vec<Point> {
             jj.map(|j| Point(i, j)).collect()
         }
     }
